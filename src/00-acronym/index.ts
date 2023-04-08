32f0392141c29546802a0f4de81a83612828b041
Andrejs Lavrinovics <andrew.lavrinovich@inbox.lv>
2023-04-08 15:54:32 +0000

    00 - done

--- src/00-acronym/index.ts
+++ src/00-acronym/index.ts
@@ -6,6 +6,16 @@
  * Help generate some jargon by writing a program that converts a long name like Portable Network Graphics to its acronym (PNG).
  */
 
-function parse(input: string) {}
+function parse(input: string): string {
+  return input
+    .split(/(?=[A-Z][*a-z]|[ ]|[-])/)
+    .map((word) =>
+      word
+        .replace(/[^0-9a-z]/gi, "")
+        .slice(0, 1)
+        .toUpperCase()
+    )
+    .join("");
+}
 
 export { parse };
